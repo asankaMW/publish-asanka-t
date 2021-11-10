12fa8a0b1703ffd8873f28aa88cad7d971eaae78
asankaMW <asanka@staff.medicalwizard.com.au>
2021-11-10 08:53:00 +0530

    second initial

--- src/TodaysDate.js
+++ src/TodaysDate.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-const TodaysDate = () => <div>{`Today's date is ${Date()}`}</div>;
+const TodaysDate = (props) => <div>{`Today's date is ${Date()}`}
+<h1>{props.value}</h1>
+</div>;
 
 export default TodaysDate;
